88cfaf7ae3c24ecbc5400832e64cb32eafe49453
w70474 <w70474@RA117B-12>
2024-10-24 14:14:31 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿int liczba = Convert.ToInt32(Console.ReadLine());
+if (liczba % 2 == 0)
+{
+    Console.WriteLine("jest parzysta");
+}
+else
+{
+    Console.WriteLine("nie jest parzysta");
+}
+for(int i = 0; i < liczba; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
