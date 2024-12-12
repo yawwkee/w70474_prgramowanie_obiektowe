7132495a78619860fd39de3128ff468b43e8856c
Pavlo Mysiuk <w70474@student.wsiz.edu.pl>
2024-12-12 14:53:03 +0100

    zad1

--- lab2-3/lab2-3/lab6/Person.cs
+++ lab2-3/lab2-3/lab6/Person.cs
@@ -1,9 +1,10 @@
-﻿using lab2_3.Interfejsy;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lab2_3.Interfejsy;
 
 namespace lab2_3.lab6
 {
