94ae1cd40280614c98c72ac46fb2e19622022db6
Pavlo Mysiuk <w70474@student.wsiz.edu.pl>
2024-12-19 14:45:01 +0100

    lab 7 niedorobione

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication1.Controllers
+namespace WebApplication2.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- WebApplication2/WeatherForecast.cs
+++ WebApplication2/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1
+namespace WebApplication2
 {
     public class WeatherForecast
     {
